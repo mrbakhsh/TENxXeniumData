2fa1c310a6cc5d606901800b4172dacef7d743aa
Matineh Rahmatbakhsh <matinerb.94@gmail.com>
2023-12-14 09:11:06 -0800

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,13 @@
-#' @importFrom utils read.csv
-#' @importFrom ExperimentHub createHubAccessors
-#' @import SpatialExperiment
-#' @import SpatialFeatureExperiment
+  #' @importFrom utils read.csv
+  #' @importFrom ExperimentHub createHubAccessors
+  #' @import SpatialExperiment
+  #' @import SpatialFeatureExperiment
+  #' @import BumpyMatrix
+  #' @import SummarizedExperiment
+  
+  .onLoad <- function(libname, pkgname) {
+    fl <- system.file("extdata", "metadata.csv", package=pkgname)
+    titles <- read.csv(fl, stringsAsFactors=FALSE)$Title
+    createHubAccessors(pkgname, titles)
+  }
 
-.onLoad <- function(libname, pkgname) {
-  fl <- system.file("extdata", "metadata.csv", package=pkgname)
-  titles <- read.csv(fl, stringsAsFactors=FALSE)$Title
-  createHubAccessors(pkgname, titles)
-}
